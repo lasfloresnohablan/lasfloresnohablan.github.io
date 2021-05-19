c0ac868419619fbdde5c8092c7970c8270af686f
lasfloresnohablan <a.rivasvillarruel@gmail.com>
2021-05-18 21:12:08 -0500

    primer prueba

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new hover.Effect ({
+	parent: document.querySelector('container')
+	intensity1: 3,
+	intensity2: 1,
+	speedIn: 1,
+	Speed2: 2,
+	image1: '../img/header3.jpg',
+	image2: '../img/header2.jpg',
+	displacementImage:'../img/header1jpg'
+})
