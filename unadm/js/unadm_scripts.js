c525bcf8e99a132315f3eaca52a2590e6b27d47d
lasfloresnohablan <38293756+lasfloresnohablan@users.noreply.github.com>
2025-02-10 13:02:24 -0600

    Update unadm_scripts.js

--- unadm/js/unadm_scripts.js
+++ unadm/js/unadm_scripts.js
@@ -30,3 +30,54 @@ fetch('unadm_pre-footer.html')
         document.getElementById('unadm-pre-footer').innerHTML = data;
     })
     .catch(error => console.error('Error loading the UNADM pre-footer section:', error));
+// accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        // Close all panels
+        for (var j = 0; j < acc.length; j++) {
+            if (acc[j] !== this) {
+                acc[j].classList.remove("active");
+                acc[j].nextElementSibling.style.display = "none";
+            }
+        }
+
+        // Toggle the clicked panel
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
